fe168eaa5dfa0172da730bf5411270d365805533
ZaidAhmedKhan-20 <zaid.ahmed@byklabs.com>
2025-09-03 15:46:26 +0500

    multi bloc provider and image picker with bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:bloc_demo/bloc/counter/counter_bloc.dart';
+import 'package:bloc_demo/bloc/image_picker/image_picker_bloc.dart';
 import 'package:bloc_demo/bloc/switch/switch_bloc.dart';
+import 'package:bloc_demo/ui/image_picker/image_picker_screen.dart';
 import 'package:bloc_demo/ui/switch_example/switch_example_screen.dart';
+import 'package:bloc_demo/utils/image_picker_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -10,23 +14,39 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   // return BlocProvider(
+  //   //   create: (_) => CounterBloc(),
+  //   //   child: const MaterialApp(
+  //   //     debugShowCheckedModeBanner: false,
+  //   //     home: CounterScreen(),
+  //   //   ),
+  //   // );
+  //   return BlocProvider(
+  //     create: (_) => SwitchBloc(),
+  //     child: BlocProvider(
+  //       create: (context) => SwitchBloc(),
+  //       child: const MaterialApp(
+  //         debugShowCheckedModeBanner: false,
+  //         home: SwitchExampleScreen(),
+  //       ),
+  //     ),
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
-    // return BlocProvider(
-    //   create: (_) => CounterBloc(),
-    //   child: const MaterialApp(
-    //     debugShowCheckedModeBanner: false,
-    //     home: CounterScreen(),
-    //   ),
-    // );
-    return BlocProvider(
-      create: (_) => SwitchBloc(),
-      child: BlocProvider(
-        create: (context) => SwitchBloc(),
-        child: const MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: SwitchExampleScreen(),
-        ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => CounterBloc()),
+        BlocProvider(create: (_) => SwitchBloc()),
+        BlocProvider(create: (_) => ImagePickerBloc(ImagePickerUtils())),
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        // home: SwitchExampleScreen(),
+        home: ImagePickerScreen(),
       ),
     );
   }
--- lib/ui/image_picker/image_picker_screen.dart
+++ lib/ui/image_picker/image_picker_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
+import 'package:bloc_demo/bloc/image_picker/image_picker_bloc.dart';
+import 'package:bloc_demo/bloc/image_picker/image_picker_event.dart';
+import 'package:bloc_demo/bloc/image_picker/image_picker_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ImagePickerScreen extends StatefulWidget {
   const ImagePickerScreen({super.key});
@@ -13,7 +19,23 @@ class _ImagePickerScreenState extends State<ImagePickerScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Image picker bloc"),
+        centerTitle: true,
+        title: Text("Image picker bloc")),
+      body: Center(
+        child: BlocBuilder<ImagePickerBloc, ImagePickerState>(
+          builder: (context, state) {
+            if (state.file == null) {
+              return InkWell(
+                onTap: () {
+                  context.read<ImagePickerBloc>().add(CameraCapture());
+                },
+                child: CircleAvatar(child: Icon(Icons.camera)),
+              );
+            } else {
+              return Image.file(File(state.file!.path.toString()));
+            }
+          },
+        ),
       ),
     );
   }
